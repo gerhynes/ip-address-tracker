67e4136c3188b901656f4b28ccc96203f813a122
Gerard Hynes <gkhynes@gmail.com>
2021-09-03 11:23:21 +0100

    Add focus styles

--- src/Form.js
+++ src/Form.js
@@ -9,14 +9,14 @@ export default function Form({ ipAddress, setIpAddress, handleSubmit }) {
       onSubmit={handleSubmit}
     >
       <input
-        className="flex-auto py-3.5 px-6 rounded-l-xl text-lg text-gray-700"
+        className="flex-auto py-3.5 px-6 rounded-l-xl text-lg text-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-200 focus:border-transparent"
         type="text"
         placeholder="Search for any IP address or domain"
         value={ipAddress}
         onChange={(e) => setIpAddress(e.target.value)}
       />
       <button
-        className="bg-black rounded-r-xl px-4 hover:bg-gray-700"
+        className="bg-black rounded-r-xl px-4 hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-200 focus:ring-opacity-50"
         aria-label="Search for IP Address"
       >
         <img src={arrowIcon} alt="" />
